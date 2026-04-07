214c882f5c9b5e870079c7b88ad5e4aa129dce30
o41-commit <oluwatobilobafatoba@gmail.com>
2026-04-07 11:04:55 +0100

    add dist and vercel routing fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    react(),          // You need this for React
     tailwindcss(),
   ],
+  base: './',         // ✅ critical for Vercel
   server: {
-    port: "3000"
+    port: 3000
   }
 })
